06ea7dc0a4bc566470ee68a27b9b3e6d9c2724df
Hamdandih <81637692+dansecret@users.noreply.github.com>
2023-01-14 16:41:33 +0700

    add apps

---
+++ apps.py
@@ -0,0 +1,31 @@
+import cv2
+
+cam = cv2.VideoCapture(1)
+
+cv2.namedWindow("Python Webcam Screenshot App")
+
+img_counter = 0
+
+while True:
+    ret,frame = cam.read()
+    
+    if not ret:
+        print("failed to grab frame")
+        break
+    
+    cv2.imshow("test",frame)
+    
+    k = cv2.waitKey(1)
+    
+    if k%256 == 27:
+        print("Escape hit, closing the app")
+        break
+    
+    elif k%256 == 32:
+        img_name = "opencv_frame_{}.png".format(img_counter)
+        cv2.imwrite(img_name,frame)
+        print("Screenshot taken")
+        img_counter+=1
+    
+cam.release()
+cam.DestroyAllWindows()
